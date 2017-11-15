02eb4ebd7b1826407d4861f43204f3ed1eb51b40
pailla3452 <nachopaillalef@gmail.com>
2017-11-15 11:15:28 +0100

    sigup + signin + router

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import Router from 'vue-router'
 
 // COMPONENTS
 import Home from '@/components/Hello'
-import Signin from '@/components/Signup.vue'
-import Signup from '@/components/Signin.vue'
+import Signin from '@/components/user/Signup.vue'
+import Signup from '@/components/user/Signin.vue'
 
 Vue.use(Router)
 
